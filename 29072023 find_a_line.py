5bd9d1994523c389802f58d9eb41c3cc34887f92
Nova-Foster <FosterN@cardiff.ac.uk>
2023-07-29 17:23:23 +0100

    Added: - Proper input for wavelengths - What the NIST File contains - Some comments that were missed

--- 29072023 find_a_line.py
+++ 29072023 find_a_line.py
@@ -2,28 +2,46 @@
 Created by Nova Foster (They/Them) 26/07/2023
 '''
 
+import pandas as pd                                               #Use pandas to read csv file
 
-import numpy as np
-import pandas as pd
-
-
+'''
+NIST File contains:
+- Element
+- Spectral Number
+- Observed wavelength in air (nm) from 250nm to 950nm
+- Observed wv. uncertainty
+- Intensity
+- Probability in terms of 10^8 s^-1 (closer to 1 is more likely)
+- Accuracy
+- Initial energy(?)
+- Final energy (?)
+- Type (mostly NA)
+- TP Reference
+- Line Reference
+'''
 NIST_Data = pd.read_csv("Lines.csv")
 
-def compare(Observed,margin=0.5,source="All"):                    #Compare observed wavelength (nm) to NIST values within range +-margin. Source being elements to select from (Main Air, All Air or All)
 
+def compare(Observed,margin=0.5,source="all"):                    #Compare observed wavelength (nm) to NIST values within range +-margin. Source being elements to select from (Main Air, All Air or All)
+  source=source.lower()
 #Select all wavelength from NIST that fit the range
-  lines = NIST_Data[NIST_Data['obs_wl_air(nm)'].between(Observed-margin,Observed+margin)]
+  lines = NIST_Data[NIST_Data['obs_wl_air(nm)'].between(int(Observed)-int(margin),int(Observed)+int(margin))]
 
 #Select the soures from that range
   #Main Air: H, He, Ar, N, O
-  if(source=="Main Air"):
+  if(source=="main air"):
     lines = lines[ (lines['element'] =="H") | (lines['element'] =="He") | (lines['element'] =="Ar") | (lines['element'] =="N") | (lines['element'] =="O")]
   #All Air: H, He, Ar, N, O, Ne, Kr, Xe, I
-  elif(source=="All Air"):
+  elif(source=="all air"):
     lines = lines[ (lines['element'] =="H") | (lines['element'] =="He") | (lines['element'] =="Ar") | (lines['element'] =="N") | (lines['element'] =="O")
     | (lines['element'] =="Ne") | (lines['element'] =="Kr") | (lines['element'] =="Xe")| (lines['element'] =="I")]
   #Else covers all possible sources
 
   return lines
 
-print(compare(712.219,5,"Main Air"))
+print("Please enter the observed wavelength(nm), margin to search (observed +-) and lines to check (Main Air, All Air or all)")
+observed = input()
+margin = input()
+source = input()
+
+print( compare(observed, margin, source) )
