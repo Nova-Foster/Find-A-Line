0f9dd90306a027fc3182eeaa3adc075455556a57
Nova-Foster <FosterN@cardiff.ac.uk>
2023-07-31 14:25:52 +0100

    Allowed for ev line file to be loaded

--- 29072023 find_a_line.py
+++ 29072023 find_a_line.py
@@ -21,7 +21,7 @@ NIST File contains:
 - Line Reference
 '''
 NIST_Data = pd.read_csv("Lines.csv")
-
+NIST_Data[""]
 
 def compare(Observed,margin=0.5,source="all"):                    #Compare observed wavelength (nm) to NIST values within range +-margin. Source being elements to select from (Main Air, All Air or All)
   source=source.lower()
