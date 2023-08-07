1245b36873d87e751ceb1dcc06bc314cbfe459a0
Nova-Foster <FosterN@cardiff.ac.uk>
2023-08-07 18:10:25 +0100

    Changes in 2line and compare for efficiency

--- 29072023 find_a_line.py
+++ 29072023 find_a_line.py
@@ -24,19 +24,26 @@ NIST File contains:
 '''
 NIST_Data = pd.read_csv("Lines_eV.csv")
 
-def compare(Observed,margin=0.5,source="all"):                    #Compare observed wavelength (nm) to NIST values within range +-margin. Source being elements to select from (Main Air, All Air or All)
-  source=source.lower()
-#Select all wavelength from NIST that fit the range
-  lines = NIST_Data[NIST_Data['obs_wl_air(nm)'].between(float(Observed)-float(margin),float(Observed)+float(margin))]
+#List of elements to be used in compare, not done in function for efficiency
+main_air_elements = {"H", "He", "Ar", "N", "O"}
+all_air_elements = {"H", "He", "Ar", "N", "O","Ne", "Kr", "Xe", "I"}
 
-#Select the soures from that range
+def compare(Observed,margin=0.5,source="all"):                    #Compare observed wavelength (nm) to NIST values within range +-margin. Source being elements to select from (Main Air, All Air or All)
+ #Change format of inputs
+  observed_float = float(Observed)
+  margin_float = float(margin)
+  source=source.lower()
+
+ #Select all wavelength from NIST that fit the range
+  lines = NIST_Data[NIST_Data['obs_wl_air(nm)'].between(observed_float-margin_float,observed_float+margin_float)]
+
+ #Select the soures from that range
   #Main Air: H, He, Ar, N, O
   if(source=="main air"):
-    lines = lines[ (lines['element'] =="H") | (lines['element'] =="He") | (lines['element'] =="Ar") | (lines['element'] =="N") | (lines['element'] =="O")]
+    lines = lines[ lines['element'].isin(main_air_elements)]
   #All Air: H, He, Ar, N, O, Ne, Kr, Xe, I
   elif(source=="all air"):
-    lines = lines[ (lines['element'] =="H") | (lines['element'] =="He") | (lines['element'] =="Ar") | (lines['element'] =="N") | (lines['element'] =="O")
-    | (lines['element'] =="Ne") | (lines['element'] =="Kr") | (lines['element'] =="Xe")| (lines['element'] =="I")]
+    lines = lines = lines[ lines['element'].isin(all_air_elements)]
   #Else covers all possible sources
 
   return lines
@@ -50,16 +57,16 @@ def temp_using_2line(matched,intensity):
   import numpy as np
   Boltzmann = 8.617333262e-5
 
-#Select values for each of the two lines and assign them to a new structure
-  line1 = NIST_Data[NIST_Data["obs_wl_air(nm)"]==matched[0]]
-  line2 = NIST_Data[NIST_Data["obs_wl_air(nm)"]==matched[1]]
+ #Select values for each of the two lines and assign them to a new structure. iloc to get them as numerical values not data frames
+  line1 = NIST_Data[NIST_Data["obs_wl_air(nm)"]==matched[0]].iloc[0]
+  line2 = NIST_Data[NIST_Data["obs_wl_air(nm)"]==matched[1]].iloc[0]
 
-#Select values using iloc[0][THING] to select single values
-  prefactor = (float(line2.iloc[0]['Ek(eV)']) - float(line1.iloc[0]['Ek(eV)'])) /Boltzmann
-  numerator = intensity[0]*line1.iloc[0]['obs_wl_air(nm)']*line2.iloc[0]['Aki(10^8 s^-1)']*line2.iloc[0]['g_k']
-  denomonator = intensity[1]*line2.iloc[0]['obs_wl_air(nm)']*line1.iloc[0]['Aki(10^8 s^-1)']*line1.iloc[0]['g_k']
+ #Calculate each part of the equation
+  prefactor = (float(line2['Ek(eV)']) - float(line1['Ek(eV)'])) /Boltzmann
+  numerator = intensity[0]*line1['obs_wl_air(nm)']*line2['Aki(10^8 s^-1)']*line2['g_k']
+  denomonator = intensity[1]*line2['obs_wl_air(nm)']*line1['Aki(10^8 s^-1)']*line1['g_k']
 
-#Calculate temperature using the equation
+ #Calculate temperature using the equation
   Temp = prefactor * np.log(numerator/denomonator)**(-1)
 
   return Temp
@@ -87,10 +94,18 @@ def boltz_line(matched,intensity):
   return Temp
 
 
+def bolt_fit(matched,intensity):
+  '''
+  TODO:
+  - Implement this using curvefit
+  - Test using Boltzmann dist. with noise and added lines
+  - Look at various windows to see what works
+  '''
+
+  return Temp
 
 
 
-'''
 print(temp_using_2line([510.5541,515.3235],[0.55,0.9]))
 
 '''
@@ -101,3 +116,4 @@ while True:
   source = input()
 
   print( compare(observed, margin, source) )
+  '''
