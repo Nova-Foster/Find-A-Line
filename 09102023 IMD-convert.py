a3d8c1cedf5b34eb4ffefba4c8a8b521e3650c95
Nova-Foster <FosterN@cardiff.ac.uk>
2023-08-10 19:58:51 +0100

    Python code loading but not converted, c++ not running properly

--- 09102023 IMD-convert.py
+++ 09102023 IMD-convert.py
@@ -1,9 +1,32 @@
 import numpy as np
 import codecs
 
-#raw_imd = open("800V - 250Hz - 500usmm - 15um vertical.imd","r",encoding="hex_codec")
-raw = codecs.open("800V - 250Hz - 500usmm - 15um vertical.imd","r",encoding="utf16")
-print(raw)
 
-test = raw.readline()
-print(test)
+stream_reader = codecs.open("20230810 - 500usmm - 800V - 250Hz -00000.IMD","rb")     #Create stream reader and open non text file
+raw = stream_reader.readlines()                                                    #Read all of the data and assign to raw
+stream_reader.close()                                                              #Close stream reader
+
+raw = str(raw)                                                                     #Convert data into a string so it can be split
+raw_split = raw.split("\\")
+
+#Header is first 6 values
+header = raw_split[:6]
+# 0 = '[b"'
+# 1 = '01'
+# 2 = '01p'
+# 3 = '05'
+# 4 = '10'
+# 5 = '04'
+
+#Data is the rest
+data = raw_split[6:]
+
+#Need to concatenate into pairs: i.e. data[1] & data[0] in a single value
+#That joined data 
+
+
+
+
+#Start of data
+# 6 = '00'
+# 7 = ....
--- c++_imd-conversion.cpp
+++ c++_imd-conversion.cpp
@@ -1,6 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
+int main(){
+    FILE *file;                                              //Define file using the FILE data type, * as it is using a pointer for some reason?
+    file = fopen("test.imd","rb");                           //open .imd  file is a streamwrited in read mode, b to say it isn't a text file
+    float m_s32data;
+    if (file){
+        unsigned short int VER,W,H;
+        // read file version
+        // read image width
+        // read image height
+        fread(&VER,sizeof(unsigned short int),1,file);
+        fread(&W,sizeof(unsigned short int),1,file);
+        fread(&H,sizeof(unsigned short int),1,file);
+        m_s32data = new signed int[W*H];
+        // retrieve the 32-bits signed int buffer
+        fread(m_s32data,sizeof(signed int),W*H,file);
+        fclose(file);
+        // If you want to create and use a floating buffer with the floating pixel value,
+        // proceed like this
+        float *fdata = new float[W*H];
+
+        for (int k=0;k<H*W;k++);
+            fdata[k] = m_s32data[k]/1000;
+            delete fdata;
+            
+            delete m_s32data;
+    }
+
     return 0;
 }
+
