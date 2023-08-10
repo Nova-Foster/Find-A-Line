ec89865983bbfc59bb3e9dc0c44a6b41912bb2e8
Nova-Foster <FosterN@cardiff.ac.uk>
2023-08-10 21:45:49 +0100

    imd loads in python & plots

--- 09102023 IMD-convert.py
+++ 09102023 IMD-convert.py
@@ -1,32 +1,25 @@
 import numpy as np
-import codecs
+import matplotlib.pyplot as py
+
+with open("800V - 250Hz - 500usmm - 15um vertical.imd", "rb") as file:      #Open the file reading as non-text
+    if file:
+        #Read first 2 bytes, least important byte first, covert to int from hex
+        VER = int.from_bytes(file.read(2), byteorder='little', signed=False) 
+        W = int.from_bytes(file.read(2), byteorder='little', signed=False)
+        H = int.from_bytes(file.read(2), byteorder='little', signed=False)
+
+        #Read the rest of the data
+        m_s32data = np.fromfile(file, dtype=np.int32, count=W * H)
+
+        #Conver the data into floats, with error handling for 0
+        fdata = np.where(m_s32data != 0, m_s32data / 1000.0, 0.0)
 
 
-stream_reader = codecs.open("20230810 - 500usmm - 800V - 250Hz -00000.IMD","rb")     #Create stream reader and open non text file
-raw = stream_reader.readlines()                                                    #Read all of the data and assign to raw
-stream_reader.close()                                                              #Close stream reader
+#Create 2d array
+rows = np.split(fdata,1040,axis=0)                                         #Split the data into each 1040 pixel row
+data_2d_array = np.stack(rows,axis=0)                                      #Stack each row on top of eachother
 
-raw = str(raw)                                                                     #Convert data into a string so it can be split
-raw_split = raw.split("\\")
-
-#Header is first 6 values
-header = raw_split[:6]
-# 0 = '[b"'
-# 1 = '01'
-# 2 = '01p'
-# 3 = '05'
-# 4 = '10'
-# 5 = '04'
-
-#Data is the rest
-data = raw_split[6:]
-
-#Need to concatenate into pairs: i.e. data[1] & data[0] in a single value
-#That joined data 
-
-
-
-
-#Start of data
-# 6 = '00'
-# 7 = ....
+#Plot the data
+py.imshow(data_2d_array,interpolation="nearest",origin="lower")
+py.colorbar()
+py.show()
--- c++_imd-conversion.cpp
+++ c++_imd-conversion.cpp
@@ -1,34 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main(){
-    FILE *file;                                              //Define file using the FILE data type, * as it is using a pointer for some reason?
-    file = fopen("test.imd","rb");                           //open .imd  file is a streamwrited in read mode, b to say it isn't a text file
-    float m_s32data;
-    if (file){
-        unsigned short int VER,W,H;
-        // read file version
-        // read image width
-        // read image height
-        fread(&VER,sizeof(unsigned short int),1,file);
-        fread(&W,sizeof(unsigned short int),1,file);
-        fread(&H,sizeof(unsigned short int),1,file);
-        m_s32data = new signed int[W*H];
-        // retrieve the 32-bits signed int buffer
-        fread(m_s32data,sizeof(signed int),W*H,file);
-        fclose(file);
-        // If you want to create and use a floating buffer with the floating pixel value,
-        // proceed like this
-        float *fdata = new float[W*H];
+int main() {
+    std::ifstream file("test.imd", std::ios::binary);
+    if (file.is_open()) {
+        unsigned short int VER, W, H;
+        file.read(reinterpret_cast<char*>(&VER), sizeof(unsigned short int));
+        file.read(reinterpret_cast<char*>(&W), sizeof(unsigned short int));
+        file.read(reinterpret_cast<char*>(&H), sizeof(unsigned short int));
 
-        for (int k=0;k<H*W;k++);
-            fdata[k] = m_s32data[k]/1000;
-            delete fdata;
-            
-            delete m_s32data;
+        std::vector<signed int> m_s32data(W * H);
+        file.read(reinterpret_cast<char*>(m_s32data.data()), sizeof(signed int) * W * H);
+        file.close();
+
+        std::vector<float> fdata(W * H);
+
+        for (int k = 0; k < H * W; k++) {
+            if (m_s32data[k] != 0) {
+                fdata[k] = static_cast<float>(m_s32data[k]) / 1000.0f;
+            } else {
+                fdata[k] = 0.0f;
+            }
+        }
+
+        // No need to manually deallocate memory with vectors
+
+    } else {
+        std::cerr << "Failed to open the file." << std::endl;
     }
-
+    
     return 0;
 }
-
