8ffa5b4fc02006fe2dafeabb4228fb96dba9d016
Nova-Foster <FosterN@cardiff.ac.uk>
2023-08-11 00:33:17 +0100

    imd conversion in main file
    plot for whole image and two profiles
    
    auto peaks implemented but needs work
    
    comments and refactoring needed all over

--- 29072023 find_a_line.py
+++ 29072023 find_a_line.py
@@ -3,6 +3,8 @@ Created by Nova Foster (They/Them) 26/07/2023
 '''
 
 import pandas as pd                                               #Use pandas to read csv file
+import matplotlib.pyplot as py
+import numpy as np
 Boltzmann = 8.617333262e-5
 
 '''
@@ -55,7 +57,6 @@ def temp_using_2line(matched,intensity):
   TODO:
   - Loop so each pair of lines is used (If the elements match)
   '''
-  import numpy as np
 
  #Select values for each of the two lines and assign them to a new structure. iloc to get them as numerical values not data frames
   line1 = NIST_Data[NIST_Data["obs_wl_air(nm)"]==matched[0]].iloc[0]
@@ -75,8 +76,6 @@ def line(x,m,c):   #Line function used for curve_fit in bolt_line
   return m*x+c
 
 def boltz_line(matched,intensity):
-  import matplotlib.pyplot as py
-  import numpy as np
   from scipy.optimize import curve_fit
 
   #Load values for matched lines into seperate array for easier handling
@@ -123,7 +122,77 @@ def boltz_fit(matched,intensity):
 
   return Temp
 
-import numpy as np
+
+def convert_imd(file_name):
+  with open(file_name, "rb") as file:      #Open the file reading as non-text
+    if file:
+        #Read first 2 bytes, least important byte first, covert to int from hex
+        VER = int.from_bytes(file.read(2), byteorder='little', signed=False) 
+        W = int.from_bytes(file.read(2), byteorder='little', signed=False)
+        H = int.from_bytes(file.read(2), byteorder='little', signed=False)
+
+        #Read the rest of the data
+        m_s32data = np.fromfile(file, dtype=np.int32, count=W * H)
+
+        #Conver the data into floats, with error handling for 0
+        fdata = np.where(m_s32data != 0, m_s32data / 1000.0, 0.0)
+
+
+  #Create 2d array
+  rows = np.split(fdata,1040,axis=0)                                         #Split the data into each 1040 pixel row
+  data_2d_array = np.stack(rows,axis=0)                                      #Stack each row on top of eachother
+
+  return data_2d_array
+
+def spark_plot(data_2d, wavelengths=0):
+
+  #Colour plot of whole image
+  py.imshow(data_2d,interpolation="nearest",origin="lower")
+  py.colorbar()
+  py.show()
+
+
+  #Horizontal profile
+  hori = np.sum(data_2d,axis=0)
+  py.plot(np.arange(0,len(hori),1),hori)
+  py.show()
+
+  #Verticle profile
+  vert = np.sum(data_2d,axis=1)
+  py.plot(np.arange(0,len(vert),1),vert)
+  py.show()
+  return
+
+
+def auto_peaks(data_2d,rel_prom=0.01,rel_width=0.15):
+  import scipy.signal as sg
+
+  #Generate horizontal profile
+  hori = np.sum(data_2d,axis=0)
+  py.plot(np.arange(0,len(hori),1),hori)
+  
+  #Use scipy to find peaks based on local maxima
+  peak_x_cords,_ = sg.find_peaks(hori)                                       #Generate X coords
+
+  peak_prominence,_,_ = sg.peak_prominences(hori,peak_x_cords)               #Find how prominent each peak is based on surroundings
+  prominent_x = peak_x_cords[peak_prominence>rel_prom*max(peak_prominence)]
+  
+ 
+  peak_width,_,_,_ = sg.peak_widths(hori,prominent_x)
+  Selected_peaks = prominent_x[peak_width>rel_width*max(peak_width)]
+
+  #Draw a line for each prominent peak
+  for i in range(len(Selected_peaks)):
+    py.plot( np.linspace(Selected_peaks[i],Selected_peaks[i],1000), np.linspace(min(hori),max(hori),1000), "r-",alpha=0.2)
+
+  py.show()
+  return
+
+data = convert_imd( "800V - 250Hz - 500usmm - 15um vertical.imd")
+#spark_plot( data )
+auto_peaks( data )
+
+'''
 lambdas = [453.0785,453.9695,510.5541,515.3235,521.8202,522.0070,529.2517]
 intens = np.array([800,800,1500,2000,1650,2500,1650])
 intens = intens / 2500
@@ -133,7 +202,7 @@ print(intens)
 
 print(boltz_line(lambdas,intens))
 
-'''
+
 print(temp_using_2line([510.5541,515.3235],[0.55,0.9]))
 '''
 '''
