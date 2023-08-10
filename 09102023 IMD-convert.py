e831ae5c12698b478e78c6ddae9c2ac4b628d57c
Nova-Foster <FosterN@cardiff.ac.uk>
2023-08-10 19:11:38 +0100

    Format files after adding c++ file

--- 09102023 IMD-convert.py
+++ 09102023 IMD-convert.py
@@ -1,1 +1,9 @@
-print("test")
+import numpy as np
+import codecs
+
+#raw_imd = open("800V - 250Hz - 500usmm - 15um vertical.imd","r",encoding="hex_codec")
+raw = codecs.open("800V - 250Hz - 500usmm - 15um vertical.imd","r",encoding="utf16")
+print(raw)
+
+test = raw.readline()
+print(test)
